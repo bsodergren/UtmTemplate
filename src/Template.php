22cb46cd854375dde2d08926074042f16085eaa6
Bsodergren <bjorn.sodergren@gmail.com>
2025-08-06 22:23:30 +0000

    fff

--- src/Template.php
+++ src/Template.php
@@ -16,8 +16,8 @@ class Template
 
     // private $regexStart = '|{';
     // private $regexEnd = '}|misu';
-    public const STYLESHEET_CALLBACK = '|{{(stylesheet)=([a-zA-Z-_/\.]+)\|?([a-zA-Z=$,.\?\{\}]+)?}}|i';
-    public const JAVASCRIPT_CALLBACK = '|{{(javascript)=([a-zA-Z-_/\.]+)\|?([a-zA-Z=$,.\?\{\}]+)?}}|i';
+    public const STYLESHEET_CALLBACK = '|{{(stylesheet)="?([\@a-zA-Z0-9-_/\.\:]+)\|?([\@\:\/a-zA-Z=$,.\?\{\}]+)?"?}}|i';
+    public const JAVASCRIPT_CALLBACK = '|{{(javascript)="?([\@a-zA-Z0-9-_/\.\:]+)\|?([\@\:\/a-zA-Z=$,.\?\{\}]+)?"?}}|i';
     public const TEMPLATE_CALLBACK = '|{{(template)=([a-zA-Z-_/\.]+)\|?(.*)?}}|i';
     public const VARIABLE_CALLBACK = '|{\$([a-zA-Z_-]+)([-+]+)?([0-9]+)?}|';
     public const LANG_CALLBACK = '|{L ([a-zA-Z_ .\?\!\,0-9]+)}|';
